148ef4d63f322a9a817459e88d6dd9138759daa6
waarith <aimoh@dervac.com>
2024-03-27 16:04:03 +0100

    Some work added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 7s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
