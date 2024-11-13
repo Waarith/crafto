81cd530bcd76ea486e65bb2cc5568a26313cb047
Jimoh Abdulwaris <156073614+Waarith@users.noreply.github.com>
2024-11-13 18:26:25 +0100

    include

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
       animation: {
         'spin-slow': 'spin 7s linear infinite',
+        marquee: 'marquee 30s linear infinite',
       }
     },
   },
